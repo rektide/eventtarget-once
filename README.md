4fc3528ce240518a452accbcabe8766b6634a08b
rektide de la faye <rektide@voodoowarez.com>
2018-02-27 00:34:34 -0500

    `eventtarget-once`.

---
+++ README.md
@@ -0,0 +1,46 @@
+# eventtarget-once
+
+> Get a promise for the first firing of an event-type firing on an event-target.
+
+# Usage
+
+`eventtarget-once`'s primary interface is a single method, expecting an `EventTarget` and a textual type to look for from the EventTarget.
+
+```
+once( document, "DOMContentLoaded").then( e=> console.log("Loaded!", e))
+```
+
+A variety of options may be passed via an options bundle, as the third argument to once. These options are documented below:
+
+
+## addEventListenerOptions
+
+`passive` (defaults to true) and `capture` are passed to the underlying `addEventListener` calls.
+
+## Filter
+
+`filter` is an optional function that transforms the received event before resolving. This is only mildly useful, as promises are pretty easy to chain transformations off of. However, if `filter` returns `false`, the current message is skipped.
+
+```
+once( myElemnt, "click", { filter: e=> e.button=== 1}).then( e=> console.log("Middle button clicked", e))
+```
+
+The `filter` function is passed first the `Event`, and second, an object with
+* `eventType` - the textual type of the event,
+* `promise` - the resulting promise,
+* `state` - an additional option passed in to once
+
+## Abort signal
+
+`signal` is an optional [AbortSignal](https://dom.spec.whatwg.org/#abortsignal) that can abort the promise (and of course cleanup all extant handler).
+
+On an abort signal event, the promise will reject. This is expected to be the only condition the promise will reject on- it ought, otherwise, wait indefinately for a valid event and resolve.
+
+## AbortError
+
+`eventtarget-once` also exports an `AbortError`, which is either
+1. the native `AbortError` if present, or
+2. it's own AbortError class, or
+3. or the AbortError set with `setAbortError`.
+
+Although all rejections are assured to be AbortErrors, this is provided such that programs can type-check errors they encounter if they are attempting more complected multi-promise systems.
---
+++ eventtarget-once.js
@@ -0,0 +1,92 @@
+/**
+  Create a promise that will resolve to the first 
+  @param eventTarget - EventTarget to attach to
+  @param eventType - eventType of the event to listen for
+  @param [options] - additional options, documented below:
+  @param [options.filter] - optional filter for return value; `false` to skip
+  @param [options.state] - supplemental data passed to a `filter` function
+  @param {AbortSignal} [options.signal] - an optional AbortSignal.
+  // addEventListenerOptions:
+  @param [options.capture] - whether to use `capture` event listening
+  @param [options.passive=true] - whether to use `passive` event listening
+  @param [options.abortPassive=options.passive] - whether to use `passive` event listening on abort signal
+  @returns {promise} the first message (or non-false filtered message) event of the given name from the EventTarget
+*/
+export function onceOn( eventTarget, eventType, options= {}){
+	var
+	  filter= options.filter,
+	  signal= options.signal,
+	  state= options.state,
+	  eventOptions= {
+		capture: options.capture,
+		once: !options.filter, // we cleanup either way, but if there is a filter, it might take multiple events
+		passive: options.passive=== undefined? true: options.passive
+	  }
+	// immediately bail out if already aborted
+	if( signal&& signal.aborted){
+		return Promise.reject(new AbortError("Aborted"));
+	}
+
+	// return promise for value, or abort
+	var promise= Promise( function( resolve, reject){
+
+		// bind handler, will call `resolve`
+		target.addEventListener( eventType, handler, eventOptions)
+		// bind abort, will call `reject`
+		if( signal){
+			// update eventOptions in place
+			eventOptions.once= true
+			eventOptions.passive= options.abortPassive!== undefined? options.abortPassive: eventOptions.passive
+			// bind abort
+			signal.addEventListener( "abort", aborter, eventOptions)
+		}
+
+		// handle events on EventTarget
+		function handler( evt){
+			// run through filter
+			if( filter){
+				try{
+					// filter, passing event, event-type, promise
+					evt= filter( evt, { eventType, promise, state})
+				}catch( ex){}
+			}
+			// filter tells us to skip this one, wait for next
+			if( filter&& msg=== false){
+				return
+			}
+
+			// cleanup
+			target.removeEventListener( eventType, handler)
+			if( signal){
+				signal.removeEventListener( "abort", aborter)
+			}
+
+			// done
+			resolve( result)
+		}
+
+		// handle abort events on the passed in AbortSignal
+		function aborter(){
+			// cleanup
+			target.removeEventListener( eventType, handler)
+			signal.removeEventListener( "abort", aborter)
+
+			// done
+			reject( new AbortError("Aborted"))
+		}
+
+	})
+	return promise
+}
+
+export function once( eventType, handler, options){
+	return onceOn( this, eventType, options
+}
+
+export let AbortError= typeof AbortError!== "undefined"? AbortError: class extends Error
+
+export function setAbortError( abortError){
+	AbortError= abortError
+}
+
+export default onceOn
